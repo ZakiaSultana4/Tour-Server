b44adb1b60af63a9fa8bf44a914f3c8473eed005
Zakia Sultana <128232277+ZakiaSultana4@users.noreply.github.com>
2024-04-29 21:45:27 +0600

    Detete from cart is added

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ async function run() {
     const result = await cartCollection.find(query).toArray();
     res.send(result);
   });
-    // Delete-----Done
+    // Delete from cart-----Done
     app.delete("/deleteC/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
